c2e4332c29975c15bd8c7799e864d917370a9156
Vlad <vladmdgolam@gmail.com>
2025-09-02 23:42:28 +0200

    Initial Commit

---
+++ emdr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  emdr
+//
+//  Created by Vlad Md Golam on 02.09.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ emdr/emdrApp.swift
@@ -0,0 +1,17 @@
+//
+//  emdrApp.swift
+//  emdr
+//
+//  Created by Vlad Md Golam on 02.09.25.
+//
+
+import SwiftUI
+
+@main
+struct emdrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
